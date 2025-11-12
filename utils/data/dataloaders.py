acede6678cfa18dabb22afe98d42ae77baa4a398
Emmanuel David Muñiz <emmanul2002@outlook.com>
2025-11-11 22:09:57 -0600

    Added code to use both datasets

--- utils/data/dataloaders.py
+++ utils/data/dataloaders.py
@@ -78,3 +78,60 @@ def create_dataloaders(chexpert_paths,
         
     return output_loader
     
+
+# from typing import Callable, Optional
+# import torch
+# from torch.utils.data import DataLoader, ConcatDataset, WeightedRandomSampler
+# from utils.data.mimic_dataset import MIMICDataset
+# from utils.data.chexpert_dataset import CHEXPERTDataset
+# from utils.processing import loader, image_transform
+
+# def create_dataloaders(chexpert_paths, 
+#                        mimic_paths, 
+#                        batch_size: int = 32,
+#                        split: str = "train", 
+#                        transform: Optional[Callable] = None,
+#                        sampling_ratio: float = 0.7,
+#                        findings_or_impression: str = "findings",
+#                        **kwargs) -> DataLoader:
+#     MIMIC_df, CHEXPERT_df = loader(chexpert_paths, mimic_paths, split=split)
+#     transform = image_transform(img_size=512) if transform is None else transform
+#     if findings_or_impression not in ["findings", "impression"]:
+#         findings_or_impression = "findings"
+#         print(f"Invalid findings_or_impression value. Defaulting to 'findings'.")
+
+#     if findings_or_impression == "findings":
+#         chexpert_ds = CHEXPERTDataset(
+#             CHEXPERT_df,
+#             chexpert_paths["chexpert_data_path"],
+#             split=split,
+#             transform=transform,
+#             text_col='section_findings',
+#         )
+
+#     elif findings_or_impression == "impression":  # "impression"
+#         chexpert_ds = CHEXPERTDataset(
+#             CHEXPERT_df,
+#             chexpert_paths["chexpert_data_path"],
+#             split=split,
+#             transform=transform,
+#             text_col='section_impression',
+#         )
+
+#     mimic_ds = MIMICDataset(
+#         MIMIC_df, 
+#         mimic_paths["mimic_images_dir"], 
+#         mimic_paths["mimic_data_path"], 
+#         transform=transform)
+    
+#     mixed = ConcatDataset([mimic_ds, chexpert_ds])
+
+#     output_loader = DataLoader(
+#         mixed,
+#         batch_size=batch_size,
+#         shuffle=True if split != "test" else False,
+#         **kwargs
+#     )
+        
+#     return output_loader
+    
